d05d8e7f03f626c4bd2d8600b562c87fdbf2bfbe
olakunlebalogun <olakunlebalogun247@gmail.com>
2022-10-06 11:20:19 +0100

    AddressRepository has been added, close #1

--- src/main/java/com/olakunle/myjpa/repository/AddressRepository.java
+++ src/main/java/com/olakunle/myjpa/repository/AddressRepository.java
@@ -2,6 +2,8 @@ package com.olakunle.myjpa.repository;
 
 import com.olakunle.myjpa.model.onetoone.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }

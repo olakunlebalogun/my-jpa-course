24d7125916aedee9dd219f855a1f43632e4ca53e
olakunlebalogun <olakunlebalogun247@gmail.com>
2022-11-08 13:00:13 +0100

    created two entities namely Employee and Department

---
+++ src/main/java/com/olakunle/myjpa/model/Department.java
@@ -0,0 +1,6 @@
+package com.olakunle.myjpa.model;
+
+public class Department {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/olakunle/myjpa/model/Employee.java
@@ -0,0 +1,9 @@
+package com.olakunle.myjpa.model;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private Long salary;
+    private String designation;
+
+}

c75fbf7ef8209e0bbbc7919123c98f239b77805e
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-01-03 14:33:32 +0100

    cleaned the project to accomodate only one to one mapping

--- src/main/java/com/olakunle/myjpa/model/UserProfile.java
+++ src/main/java/com/olakunle/myjpa/model/UserProfile.java
@@ -37,8 +37,6 @@ public class UserProfile {
     private User user;
 
 
-//    user_id
-
 
 
 }
--- src/main/java/com/olakunle/myjpa/repository/EmployeeDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.olakunle.myjpa.repository;
-
-import com.olakunle.myjpa.model.EmployeeDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeDetailsRepository extends JpaRepository<EmployeeDetails, Long> {
-
-
-}
